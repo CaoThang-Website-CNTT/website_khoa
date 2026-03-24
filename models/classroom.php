a4b31a6e6ab7433f3e3d7606d070fa2f35f75cb9
KhoaTr197 <darkreaper197@gmail.com>
2026-03-24 21:53:59 +0700

    feat(review/classroom): Migrate SQL Script, models, service của `classroom`

--- models/classroom.php
+++ models/classroom.php
@@ -6,9 +6,20 @@ class Classroom
 {
   public function __construct(
     public int $id,
-    public string $name,
-    public ?string $description,
-  ) {}
+    public string $level,
+    public int $major_id,
+    public int $class_of,
+    public ?int $specialization_id,
+    public string $letter,
+    public string $short_name,
+    public ?string $created_at = null,
+    public ?string $updated_at = null,
+    public ?string $deleted_at = null,
+
+    public ?Major $major = null,
+    public ?Specialization $specialization = null,
+  ) {
+  }
   /**
    * Tự động mapping trường dữ liệu DB
    * @param array $data
@@ -18,8 +29,18 @@ class Classroom
   {
     return new self(
       id: $data['id'] ?? 0,
-      name: $data['name'] ?? '',
-      description: $data['description'] ?? ''
+      level: $data['level'] ?? '',
+      major_id: $data['major_id'] ?? 0,
+      class_of: $data['class_of'] ?? 0,
+      specialization_id: $data['specialization_id'] ?? 0,
+      letter: $data['letter'] ?? '',
+      short_name: $data['short_name'] ?? '',
+      created_at: $data['created_at'] ?? null,
+      updated_at: $data['updated_at'] ?? null,
+      deleted_at: $data['deleted_at'] ?? null,
+
+      major: isset($data['maj_full_name']) ? Major::fromArray($data, 'maj_') : null,
+      specialization: isset($data['spe_full_name']) ? Specialization::fromArray($data, 'spe_') : null
     );
   }
 }
