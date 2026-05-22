62137b71b02c241a1023f62d0adc45079a638754
KhoaTr197 <darkreaper197@gmail.com>
2026-05-22 17:47:05 +0700

    refactor(feature/post): Thêm default vào `media.title` và comments

--- database/migrations/2026_04_09_002207_create_media_table.php
+++ database/migrations/2026_04_09_002207_create_media_table.php
@@ -12,19 +12,20 @@ return new class extends BaseMigration {
     $schema->create('media', function (TableBuilder $table) {
       $table->id();
 
-      $table->varchar('title', 255);
-      $table->varchar('file_name', 255);
-      $table->varchar('file_path', 1000);
+      $table->varchar('title', 255)->default("")->comment("Tiêu đề cho media (Optional)");
+      $table->varchar('file_name', 255)->comment("Hệ thông tự tạo tên cho unique");
+      $table->varchar('file_path', 1000)->comment("Đường dẫn tương đối");
       $table->varchar('mime_type', 100);
-      $table->varchar('alt_text', 255)->nullable();
+      $table->varchar('alt_text', 255)->nullable()->comment("Tối ưu SEO, aria");
 
       // Tối ưu truy vấn và tối ưu render cho image, video 
       // Các mime_type khác không đụng tới
-      $table->int('width')->unsigned()->nullable()->default(null);
-      $table->int('height')->unsigned()->nullable()->default(null);
+      $table->int('width')->unsigned()->nullable()->default(null)->comment("Kích thước cho media dạng image/video, các media khác không cần đụng tới");
+      $table->int('height')->unsigned()->nullable()->default(null)->comment("Kích thước cho media dạng image/video, các media khác không cần đụng tới");
 
       $table->bigInt('file_size')->unsigned();
-      $table->json('metadata')->nullable()->default(null);
+      $table->json('metadata')->nullable()->default(null)->comment("
+      JSON lưu thêm metadata chuyên dụng cho từng loại media");
 
       $table->timestamps();
 
