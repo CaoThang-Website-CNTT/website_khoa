88e571d82c37dd03014a27c20b9bd67123fd3466
Huy Huynh <hkhuy2208@gmail.com>
2026-06-17 19:49:25 +0700

    fix(merge/responsive_export_table): thông báo lỗi icon

--- templates/partials/dashboard_head.php
+++ templates/partials/dashboard_head.php
@@ -3,7 +3,7 @@
 <title>Cao Thang CNTT Dashboard</title>
 
 <!-- ========== All CSS files linkup ========= -->
-<link rel="icon" type="image/png" sizes="32x32" href="public/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= url('public/favicon-32x32.png') ?>">
 <link rel="preload" as="style" href="<?= url('public/css/fonts.css') ?>">
 <link rel="preload" as="style" href="<?= url('public/css/base.css') ?>">
 <link rel="preload" as="style" href="<?= url('public/css/common.css') ?>">
@@ -17,9 +17,6 @@
 <link rel="stylesheet" href="<?= url('public/css/common.css') ?>">
 <link rel="stylesheet" href="<?= url('public/css/dashboard.css') ?>">
 <link rel="stylesheet" href="<?= url('public/css/dnd.css') ?>">
-
-
-<link rel="shortcut icon" href="<?= url('assets/images/favicon.svg') ?>" type="image/x-icon" />
 
 <script src="<?= url('/public/js/toast.js') ?>"></script>
 <script src="<?= url('/public/js/dnd.js') ?>"></script>
